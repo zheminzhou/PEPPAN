04e955f8acc8e1350074de0ea2763b6abb3f5438
Zhemin Zhou <zhemin.zhou@warwick.ac.uk>
2020-04-24 23:23:43 +0100

    get back py postfix for pip installation

--- example.bash
+++ example.bash
@@ -1,6 +1,6 @@
 # generate pan-genome prediction using PEPPA
-PEPPA -P examples/GCF_000010485.combined.gff.gz --min_cds 60 --incompleteCDS s -p examples/ST131 examples/*.gff.gz
+PEPPA -p examples/ST131 -P examples/GCF_000010485.combined.gff.gz examples/*.gff.gz
 
 # generate summaries for PEPPA predicted CDSs and pseudogenes
-PEPPA_parser -g examples/ST131.PEPPA.gff -s examples/PEPPA_out -m -t -c -a 95 
+PEPPA_parser -g examples/ST131.PEPPA.gff -s examples/PEPPA_out -m -t -c -a 95
 
